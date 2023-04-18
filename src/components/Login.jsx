e7b4c97a4cac989499a4aa467ae4ccbfe2edb00f
Kaniel D. Tapper <kanieldannn@gmail.com>
2023-04-18 10:27:59 -0400

    Created Login-Logout page

---
+++ src/components/Login.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function Login() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    if(isLoggedIn) {
+        return (
+            <section>
+                <h1>Welcome Back!</h1>
+                <button onClick={() => setIsLoggedIn(false)} >Logout</button>
+            </section>
+        )
+    }
+    return ( 
+    <section>
+        <h1>Login</h1>
+        <button onClick={() =>setIsLoggedIn(true)} >Login</button>
+    </section> 
+    )
+
+}
---
+++ src/components/Message.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react"
+
+export default function Message() {
+    const [message, setMessage] = useState("Inital Message")
+    useEffect(() => {
+    }, [])
+
+    return  <h2>{message}</h2>
+}
